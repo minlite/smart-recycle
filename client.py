beb28559858d5ee4000996e02a730a6c0111ab13
adamklas <aklas@ucsd.edu>
2019-04-29 20:34:51 -0700

    added server and client files

---
+++ client.py
@@ -0,0 +1,138 @@
+"""
+ECE 196 Face Recognition Project
+Author: Will Chen, Simon Fong
+
+What this script should do:
+1. Start running the camera.
+2. Detect a face, display it, and get confirmation from user.
+3. Send it for classification and fetch result.
+4. Show result on face display.
+"""
+
+import time
+import cv2
+import base64
+import requests
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+
+# Font that will be written on the image
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+
+# TODO: Declare path to face cascade
+CASCADE_PATH = "/home/pi/opencv-2.4.13.4/data/haarcascades/haarcascade_frontalface_default.xml"
+
+
+def request_from_server(img):
+    """
+    Sends image to server for classification.
+
+    :param img: Image array to be classified.
+    :returns: Returns a dictionary containing label and cofidence.
+    """
+    # URL or PUBLIC DNS to your server
+    URL = "http://ec2-54-71-200-251.us-west-2.compute.amazonaws.com:8080/predict"
+
+    # File name so that it can be temporarily stored.
+    temp_image_name = 'temp.jpg'
+
+    # TODO: Save image with name stored in 'temp_image_name'
+    cv2.imwrite(temp_image_name, img)
+    # Reopen image and encode in base64
+    # Open binary file in read mode
+    image = open(temp_image_name, 'rb')
+    image_read = image.read()
+    image_64_encode = base64.encodestring(image_read)
+
+    # Defining a params dict for the parameters to be sent to the API
+    payload = {'image': image_64_encode}
+
+    # Sending post request and saving the response as response object
+    response = requests.post(url=URL, json=payload)
+
+    # Get prediction from response
+    prediction = response.json()
+
+    return prediction
+
+
+def main():
+    # 1. Start running the camera.
+    # TODO: Initialize face detector
+    face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
+    # Initialize camera and update parameters
+    camera = PiCamera()
+    width = 640
+    height = 480
+    #camera.rotation = 180
+    camera.resolution = (width, height)
+    rawCapture = PiRGBArray(camera, size=(width, height))
+
+    # Warm up camera
+    print 'Let me get ready ... 2 seconds ...'
+    time.sleep(2)
+    print 'Starting ...'
+
+    # 2. Detect a face, display it, and get confirmation from user.
+    for frame in camera.capture_continuous(
+                    rawCapture,
+                    format='bgr',
+                    use_video_port=True):
+
+        # Get image array from frame
+        frame = frame.array
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # TODO: Use face detector to get faces.
+        # Be sure to save the faces in a variable called 'faces'
+	faces = face_cascade.detectMultiScale(img,1.3,5)
+
+        for (x, y, w, h) in faces:
+            print('==================================')
+            print('Face detected!')
+            cv2.imshow('Face Image for Classification', frame)
+	    image_crop = img[y:(y+h), x:(x+w)]
+            image_crop = cv2.resize(image_crop,(224,224))
+
+            # Keep showing image until a key is pressed
+            cv2.waitKey()
+            answer = input('Confirm image (1-yes / 0-no): ')
+            print('==================================')
+
+            if(answer == 1):
+                print('Let\'s see who you are...')
+
+                # TODO: Get label and confidence using request_from_server
+		prediction = request_from_server(image_crop)
+                confidence = prediction["confidence"]
+		label = prediction["label"]
+                print('New result found!')
+
+                # TODO: Display label on face image
+                # Save what you want to write on image to 'result_to_display'
+                # [OPTIONAL]: At this point you only have a number to display,
+                # you could add some extra code to convert your number to a
+                # name
+                if int(label)==17:
+			result_to_display = 'Hello Miro!'	
+                elif int(label)== 18:
+			result_to_display = 'Hello Adam!'
+                else:
+			result_to_display = label
+		
+
+                cv2.putText(frame, str(result_to_display + ", conf: " + str(confidence)), (10, 30), FONT, 1, (0, 255, 0), 2)
+                cv2.imshow('Face Image for Classification', frame)
+                cv2.waitKey()
+                break
+
+        # Delete image in variable so we can get the next frame
+        rawCapture.truncate(0)
+
+        print('Waiting for image...')
+        time.sleep(1)
+
+
+# Runs main if this file is run directly
+if(__name__ == '__main__'):
+    main()
---
+++ server.py
@@ -0,0 +1,206 @@
+"""
+server.py
+
+ECE196 Face Recognition Project
+Author: Will Chen, Simon Fong
+
+What this script should do:
+1. Load a model with saved weights.
+2. Create a webserver.
+3. Handle classification requests:
+    3.1 Save the image from the request.
+    3.2 Load the image and classify it.
+    3.3 Send the label and confidence back to requester(Pi).
+
+Installation:
+    pip install numpy keras tensorflow h5py flask flask-cors
+"""
+import cv2
+import numpy as np
+from keras.models import load_model
+from flask import Flask, request, jsonify, abort, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)                               # Allow CORS (Cross Origin Requests)
+
+# TODO: Load the model from the weights file.
+MODEL = load_model('./vgg16_new_version_weights.h5')
+
+
+def classify(path_to_image):
+    """
+    Classify a face image
+    :param path_to_image: Path of face image
+    :return: label and confidence in a dictionary
+    """
+
+    # Image dimensions that the model expects
+    img_height, img_width, num_channel = 224, 224, 3
+
+    # Used for VGG16 to normalize the images
+    mean_pixel = np.array([104., 117., 123.]).reshape((1, 1, 3))
+
+    # TODO: Use opencv to read and resize image to standard dimensions
+    img =  cv2.imread(path_to_image,0)
+    resized_img = cv2.resize(img,(img_height,img_width))
+
+    # TODO: Subtract mean_pixel from the image store the new image in
+    # a variable called 'normalized_image'
+    normalized_image =  resize_img - mean_pixel
+
+    # Turns image shape of (2,) to (1,2)
+    image_to_be_classified = np.expand_dims(normalized_image, axis=0)
+
+    # TODO: Use network to predict the 'image_to_be_classified' and
+    # get an array of prediction values
+    # Note: MODEL.predict() returns an array of arrays ie. [[classes]]
+    predictions =  MODEL.predict(image_to_be_classified)
+
+    # TODO: Get the predicted label which is defined as follows:
+    # Label = the index of the largest value in the prediction array
+    # This label is a number, which corresponds to the same number you
+    # give to the folder when you organized data
+    # Hint: np.argmax
+    label = np.argmax(predictions)
+
+    # TODO: Calculate confidence according to the following metric:
+    # Confidence = prediction_value / sum(all_prediction_values)
+    # Be sure to call your confidence value 'conf'
+    # Hint: np.sum()
+    label_value = predictions[label]
+    total =  np.sum(predictions)
+    conf =  label_value/total
+
+    prediction = {'label': str(label),
+                  'confidence': float(conf)}
+
+    return prediction
+
+
+@app.route('/')
+def index():
+    """
+    Handles sending the webcam tool.
+    """
+    return send_from_directory('.', 'index.html')
+
+
+# Persons seen list. Contains prediction dictionaries of label and confidence.
+persons = []
+
+
+@app.route('/person/<person>')
+def get_person(person):
+    """ Handles returning persons last seen.
+
+    If a integer is given, return the the person in that index of the array.
+    If the string 'last', return the last person seen.
+    If anything else, return an error.
+
+    :param person: index | last | other
+    :return: prediction | error
+    """
+
+    # Default prediction to return. If nobody has been logged yet.
+    prediction = {'error': 'No body seen yet.'}
+
+    # Default index
+    index = None
+
+    # Handle specific index
+    try:
+        index = int(person)
+
+    except ValueError as e:
+        print("Exception: {}".format(e))
+
+    # Handle last person
+    if(person == 'last'):
+        index = -1
+
+    # Handle out of bounds.
+    try:
+        prediction = persons[index]
+    except IndexError as e:
+        # When index is not in range.
+        print("Exception: {}".format(e))
+    except TypeError as e:
+        # When index is None
+        print("Exception: {}".format(e))
+
+    # Converts python dictionary into JSON format
+    prediction_json = jsonify(prediction)
+
+    # Respond to the request (Send prediction back to Pi)
+    return prediction_json
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    """Receives an image, classifies the image, and responds with the label."""
+
+    image = None
+
+    # This extracts the image data from the request
+    if(request.method == 'POST'):
+        if('image' not in request.form and 'image' not in request.json):
+            print(request.form)
+            abort(400)
+        try:
+            image = request.json['image']
+        except(TypeError):
+            image = request.form['image']
+
+    starter = image.find(',')
+    image_data = image[starter+1:]
+
+    # Path where the image will be saved
+    temp_image_name = 'temp.jpg'
+
+    # Decodes the image data and saves the image to disk
+    with open(temp_image_name, 'wb') as fh:
+        fh.write(image_data.decode('base64'))
+
+    # TODO: Call classify to predict the image and save the result to a
+    # variable called 'prediction'
+    prediction = classify(temp_image_name)
+
+    # Add to persons list
+    persons.append(prediction)
+
+    # Converts python dictionary into JSON format
+    prediction_json = jsonify(prediction)
+
+    # Respond to the request (Send prediction back to Pi)
+    return prediction_json
+
+
+def main(args):
+    app.run(
+        host='0.0.0.0',
+        port=args.port,
+        threaded=False,
+        debug=args.debug,
+        ssl_context=args.ssl)
+
+
+if(__name__ == "__main__"):
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port',
+                        help="Port that the server will run on.",
+                        type=int,
+                        default=8080)
+    parser.add_argument('-d', '--debug',
+                        help="Whether or not to run in debug mode.",
+                        default=False,
+                        action='store_true')
+    parser.add_argument('-s', '--ssl',
+                        help="Whether or not to run with HTTPS",
+                        default=None,
+                        action='store_const',
+                        const='adhoc')
+
+    args = parser.parse_args()
+    main(args)
